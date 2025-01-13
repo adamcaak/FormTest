4a19c4e70cd5afa7af3ab909b7f6513d0d844d9d
adamcaak <ladamczak1997@gmail.com>
2025-01-13 21:20:13 +0100

    Initial Commit

---
+++ FormTest/FormTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormTestApp.swift
+//  FormTest
+//
+//  Created by Łukasz Adamczak on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
