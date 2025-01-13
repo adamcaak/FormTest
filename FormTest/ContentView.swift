43b2035a28e03769a0fc0c752f10975a1e40a8ab
adamcaak <ladamczak1997@gmail.com>
2025-01-13 22:05:07 +0100

    Add Picker

--- FormTest/ContentView.swift
+++ FormTest/ContentView.swift
@@ -13,27 +13,35 @@ struct ContentView: View {
     @State private var message: String = "Write your message..."
     @State private var include: Bool = true
     @State private var hasNotfications: Bool = false
+    @State private var help = "Help"
     
     
     var body: some View {
-        Form {
-            Section(header: Text("Personal Information")) {
-                TextField("Name:", text: $name)
-                TextField("Email:",  text: $email)
-            }
-            Section(header: Text("Message")) {
-                TextEditor(text: $message)
-            }
-            Section(header: Text("Options")) {
-                Toggle("Include Logs", isOn: $include)
-                Toggle(isOn: $hasNotfications){
-                    Text("Notifications")
+        NavigationView() {
+            Form {
+                Section(header: Text("Personal Information")) {
+                    TextField("Name:", text: $name)
+                    TextField("Email:",  text: $email)
                 }
-            }
-            Section(footer: Text("Send form")) {
-                Button("Submit", action: {
-                    print("Tapped Button")
-                })
+                Section(header: Text("Message")) {
+                    TextEditor(text: $message)
+                }
+                Section(header: Text("Options")) {
+                    Toggle("Include Logs", isOn: $include)
+                    Toggle(isOn: $hasNotfications){
+                        Text("Notifications")
+                    }
+                }
+                Section(footer: Text("Send form")) {
+                    Button("Submit", action: {
+                        print("Tapped Button")
+                    })
+                }
+                Picker("Subject", selection: $help) {
+                    ForEach(["Help", "Support", "Bug Report"], id: \.self) { subject in
+                        Text(subject)
+                    }
+                }
             }
         }
     }
