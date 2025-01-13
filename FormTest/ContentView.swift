f1c2f8565b6f1fa10bd15ee72681b616efc98fa9
adamcaak <ladamczak1997@gmail.com>
2025-01-13 22:23:43 +0100

    Add
    Add contact method, switch, new picker style, nav title

--- FormTest/ContentView.swift
+++ FormTest/ContentView.swift
@@ -10,39 +10,63 @@ import SwiftUI
 struct ContentView: View {
     @State private var name: String = ""
     @State private var email: String = ""
+    @State private var phone: String = ""
     @State private var message: String = "Write your message..."
     @State private var include: Bool = true
     @State private var hasNotfications: Bool = false
     @State private var help = "Help"
+    @State private var preferedContactMethod = ContactMethod.email
     
+    
+    enum ContactMethod {
+        case email
+        case phone
+    }
     
     var body: some View {
-        NavigationView() {
+        NavigationView {
             Form {
                 Section(header: Text("Personal Information")) {
                     TextField("Name:", text: $name)
-                    TextField("Email:",  text: $email)
                 }
+                
+                Section(header: Text("Conntact Method")) {
+                    Picker("Contact Method", selection: $preferedContactMethod) {
+                        Text("Email:").tag(ContactMethod.email)
+                        Text("Phone:").tag(ContactMethod.phone)
+                    }
+                    .pickerStyle(.segmented)
+                    switch preferedContactMethod {
+                        case .email:
+                        TextField("Email:", text: $email)
+                    case .phone:
+                        TextField("Phone:", text: $phone)
+                    }
+                }
+                
                 Section(header: Text("Message")) {
                     TextEditor(text: $message)
                 }
+                
                 Section(header: Text("Options")) {
                     Toggle("Include Logs", isOn: $include)
                     Toggle(isOn: $hasNotfications){
                         Text("Notifications")
                     }
                 }
+                
                 Section(footer: Text("Send form")) {
                     Button("Submit", action: {
                         print("Tapped Button")
                     })
                 }
+                
                 Picker("Subject", selection: $help) {
                     ForEach(["Help", "Support", "Bug Report"], id: \.self) { subject in
                         Text(subject)
                     }
                 }
-            }
+            }.navigationTitle("Contact Us")
         }
     }
 }
