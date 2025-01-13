26f45581a0dd85c89b6cf9b6a79fbaea49b804ae
adamcaak <ladamczak1997@gmail.com>
2025-01-13 21:46:15 +0100

    Add button

--- FormTest/ContentView.swift
+++ FormTest/ContentView.swift
@@ -24,6 +24,9 @@ struct ContentView: View {
             Toggle(isOn: $hasNotfications){
                 Text("Notifications")
             }
+            Button("Submit", action: {
+                print("Tapped Button")
+            })
         }
     }
 }
